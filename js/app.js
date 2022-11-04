d5d478df7054de5a5ddeb23d809a7e13b8d1cf06
ianchan3 <ianchan3138@gmail.com>
2022-11-04 15:36:26 -0700

    Day 18 of 100 Days of JavaScript Completed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
-let time = 10;
-let promotime = time * 60;
+let time = 5000;
+let promoTime = time * 60;
 
 let counting = document.getElementById("countdown");
 
+function startCountdown() {
+  let promoTimer = setInterval(() => {
+    if (promoTime <= 0) {
+      clearInterval(promoTimer);
+      counting.innerHTML = "Promotion has ended"
+    } else {
+      promoTime--;
+      const days = Math.floor(promoTime / 3600 / 24);
+      const hours = Math.floor(promoTime / 3600) % 24;
+      const minutes = Math.floor(promoTime / 60) % 60;
+      const seconds = Math.floor(promoTime % 60) ;
+      counting.innerHTML = `Time Left:<br>${days} Days | ${hours} Hrs | ${minutes} Min | ${seconds}s`
+    }
+  }, 1000)
+}
 
-
-
-const days = Math.floor(totalSeconds / 3600 / 24);
-const hours = Math.floor(totalSeconds / 3600) % 24;
-const minutes = Math.floor(totalSeconds / 60) % 60;
-const seconds = Math.floor(totalSeconds) % 60;
+startCountdown();
